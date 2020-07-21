666f4730ab04a59b2f5fa6a9b5c33a60c6ae2459
Evan Pavone <evanrpavone@gmail.com>
2020-07-21 14:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!'
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
-  return "It's ${days} days until ${holiday}!"
+  return `It\'s ${days} days until ${holiday}!`
 }
